d2b8c135ab9907d4ffa95a32a8ce45a08a366588
ahmaddhr <ahmaddaherr004@gmail.com>
2025-08-19 10:52:09 +0300

    fix

--- app/api/dashboard/top-products/route.js
+++ app/api/dashboard/top-products/route.js
@@ -10,21 +10,30 @@ export async function GET() {
     const topProducts = await Product.find({})
       .sort({ nbOfOrders: -1 })
       .limit(5)
-      .select("name nbOfOrders price img");
+      .select("name nbOfOrders price img quantity");
 
     return NextResponse.json({
       success: true,
       topProducts,
+      {
+        status: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET",
+        },
+      }
     });
-  } catch (error) {
-    console.error("Error fetching top products:", error);
-    return NextResponse.json(
-      { success: false, message: "Failed to fetch top products" },
-      { status: 500 },
+} catch (error) {
+  console.error("Error fetching top products:", error);
+  return NextResponse.json(
+    { success: false, message: "Failed to fetch top products" },
+    {
+      status: 500,
       headers: {
-      "Access-Control-Allow-Origin": "*", // or "https://yourdomain.com"
-      "Access-Control-Allow-Methods": "GET",
-    },
-    );
-  }
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET",
+      },
+    }
+  );
+}
 }
